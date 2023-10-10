a01723b66da26a92e78c993fa4c454853bb2f2ff
ArielTintel <arielandrade97@gmail.com>
2023-10-10 08:38:52 -0300

    Configurado Eureka Clients

--- api-payroll/src/main/java/com/arieltintel/apipayroll/ApiPayrollApplication.java
+++ api-payroll/src/main/java/com/arieltintel/apipayroll/ApiPayrollApplication.java
@@ -2,12 +2,13 @@ package com.arieltintel.apipayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-@RibbonClient(name = "api-worker")
 public class ApiPayrollApplication {
 
 	public static void main(String[] args) {
