4973906895e58de385067f88be2a16d53ae0f248
ArielTintel <arielandrade97@gmail.com>
2023-10-12 12:07:43 -0300

    Hystrix tolerancia a falhas (CircuitBreaker)

--- api-payroll/src/main/java/com/arieltintel/apipayroll/controller/PaymentController.java
+++ api-payroll/src/main/java/com/arieltintel/apipayroll/controller/PaymentController.java
@@ -2,7 +2,9 @@ package com.arieltintel.apipayroll.controller;
 
 import com.arieltintel.apipayroll.entities.Payment;
 import com.arieltintel.apipayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
+@Log4j2
 @RestController
 @RequestMapping("v1/payments")
 @RequiredArgsConstructor
@@ -17,11 +22,24 @@ public class PaymentController {
 
     private final PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/worker/{workerId}/days/{daysWorked}")
     @ResponseStatus(HttpStatus.OK)
     public Payment getPayment(@PathVariable(value = "workerId") Long workerId,
                               @PathVariable(value = "daysWorked")Integer daysWorked) {
         return paymentService.getPayment(workerId, daysWorked);
+    }
+
+    /**
+     * Alternative implementation, dependent on the business rule.
+     */
+    public Payment getPaymentAlternative(Long workerId, Integer days) {
+        log.info("################ Executed alternative method");
+        return Payment.builder()
+                .name("Ariel Tintel")
+                .dailyIncome(new BigDecimal("2725.49"))
+                .daysWorked(days)
+                .build();
     }
 
 }
