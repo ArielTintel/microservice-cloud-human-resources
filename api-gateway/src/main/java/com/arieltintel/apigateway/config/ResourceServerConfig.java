83ee8b9a4b761a1418b92331a1e26129cb72da3e
Ariel Tintel <arielandrade97@gmail.com>
2023-11-11 23:25:52 -0300

    Configuracao de CORS

--- api-gateway/src/main/java/com/arieltintel/apigateway/config/ResourceServerConfig.java
+++ api-gateway/src/main/java/com/arieltintel/apigateway/config/ResourceServerConfig.java
@@ -1,13 +1,22 @@
 package com.arieltintel.apigateway.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @EnableResourceServer
 @Configuration
@@ -16,6 +25,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 
+	private static final String ROLE_ADMIN = "ADMIN";
+	private static final String ROLE_OPERATOR = "OPERATOR";
 	private static final String[] PUBLIC_PATH = { "/api-oauth/oauth/token" };
 	private static final String[] OPERATOR_PATH = { "/api-worker/**" };
 	private static final String[] ADMIN_PATH = { "/api-payroll/**", "/api-user/**", "/actuator/**", "/api-worker/actuator/**", "/api-oauth/actuator/**" };
@@ -29,8 +40,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(PUBLIC_PATH).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_PATH).hasAnyRole("OPERATOR", "ADMIN")
-		.antMatchers(ADMIN_PATH).hasRole("ADMIN")
+		.antMatchers(HttpMethod.GET, OPERATOR_PATH).hasAnyRole(ROLE_OPERATOR, ROLE_ADMIN)
+		.antMatchers(ADMIN_PATH).hasRole(ROLE_ADMIN)
 		.anyRequest().authenticated();
+
+		http.cors().configurationSource(corsConfigurationSource());
 	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOrigins(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> corsFilter = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		corsFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return corsFilter;
+	}
+
 }
